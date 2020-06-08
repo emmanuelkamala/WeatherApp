aa236afdeff706ff115ec892c3af7ba65aca83fa
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2020-06-08 17:42:03 +0300

    Add UI module functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,17 @@
 const weather = new Weather('Boston');
+const ui = new UI;
+
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    
+    weather.changeLocation('Miami');
+
+    getWeather();
+
+    $('#locModal').modal('hide');
+
+})
 
 
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -6,7 +19,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
     weather.getWeather()
     .then(results => {
-        console.log(results);
+        ui.paint(results);
     })
     .catch(err => console.log(err));
 
---
+++ assets/scripts/ui.js
@@ -0,0 +1,25 @@
+class UI {
+    constructor(){
+        this.location = document.getElementById('w-location');
+        this.desc = document.getElementById('w-desc');
+        this.string = document.getElementById('w-string');
+        this.details = document.getElementById('w-details');
+        this.icon = document.getElementById('w-icon');
+        this.humidity = document.getElementById('w-humidity');
+        this.feelsLike = document.getElementById('w-feels-like');
+        this.pressure = document.getElementById('w-pressure');
+        this.wind = document.getElementById('w-wind');
+    }
+
+    paint(weather) {
+        this.location.textContent = weather.name;
+        this.desc.textContent = weather.weather[0].description;
+        this.string.textContent = weather.main.temp;
+        this.icon.setAttribute('src', weather.weather[0].icon);
+        this.humidity.textContent = `Relative humidity: ${weather.main.humidity}`;
+        this.feelsLike.textContent = `Feels like: ${weather.main.feels_like}`;
+        this.pressure.textContent = `Pressure: ${weather.main.pressure}`;
+        this.wind.textContent = `Wind: ${weather.wind.deg}`;
+        
+    }
+}
