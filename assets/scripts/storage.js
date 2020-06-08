154b45ef95d1a44a4ecd016bb3ce8937be7b4b0b
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2020-06-08 17:51:29 +0300

    Create a local storage

---
+++ assets/scripts/storage.js
@@ -0,0 +1,22 @@
+class Storage {
+    constructor(){
+        this.city;
+        this.defaultCity = 'Dar es salaam';
+    }
+
+    getLocationData(){
+        if (localStorage.getItem('city') === null){
+            this.city = this.defaultCity;
+        } else {
+            this.city = localStorage.getItem('city');
+        }
+
+        return {
+            city: this.city;
+        }
+    }
+
+    setLocationData(city){
+      localStorage.setItem('city', city);
+    }
+}
